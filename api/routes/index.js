732178b5ba561f15c41150a519fef38e61537a49
a-fadlly <a.fadlly@yahoo.com>
2020-04-03 17:02:55 +0700

    commit

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,18 @@
 import auth from '../controllers/authController';
+import todos from '../controllers/todosController';
+import authorize from '../middlewares/authorize';
 
-export const routes = (app) => {
-    app.get('/', (req, res) => res.send({ message: 'Welcome to Todo API' }));
+const routes = (app) => {
+  app.get('/', (req, res) => res.send({ message: 'Welcome to Todo API' }));
 
-    app.post('/api/auth/sign_up', auth.signUp);
-}
+  app.post('/api/auth/sign_up', auth.signUp);
+  app.post('/api/auth/sign_in', auth.signIn);
+
+  app.post('/api/todos', authorize, todos.create);
+  app.get('/api/todos', authorize, todos.fetchAll);
+  app.get('/api/todos/:todoId', authorize, todos.fetchOne);
+  app.put('/api/todos/:todoId', authorize, todos.update);
+  app.delete('/api/todos/:todoId', authorize, todos.delete);
+};
+
+export default routes;
