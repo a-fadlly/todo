30c2ef4949b69bfd4b9010e90d058b4ebe00cec6
a-fadlly <a.fadlly@yahoo.com>
2020-04-03 16:48:33 +0700

    commit

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -4,18 +4,18 @@ import { hashPassword, jwtToken } from '../utils';
 const { User } = models;
 
 const auth = {
-    async signUp(req, res, next) {
-        try {
-            const { name, email, password } = req.body;
-            const hash = hashPassword(password);
-            const user = await User.create({ name, email, password: hash });
-            const token = jwtToken.createToken(user);
-            const { id } = user;
-            return res.status(201).send({ token, user: { id, name, email } });
-        } catch (e) {
-            return next(new Error(e));
-        }
-    },
+  async signUp(req, res, next) {
+    try {
+      const { name, email, password } = req.body;
+      const hash = hashPassword(password);
+      const user = await User.create({ name, email, password: hash });
+      const token = jwtToken.createToken(user);
+      const { id } = user;
+      return res.status(201).send({ token, user: { id, name, email } });
+    } catch (e) {
+      return next(new Error(e));
+    }
+  }
 };
 
 export default auth;
---
+++ api/models/todoitem.js
@@ -0,0 +1,15 @@
+export default (sequelize, DataTypes) => {
+  const TodoItem = sequelize.define('TodoItem', {
+    text: DataTypes.STRING,
+    todoId: DataTypes.INTEGER,
+    isCompleted: DataTypes.BOOLEAN
+  }, {});
+  TodoItem.associate = (models) => {
+    // associations can be defined here
+    TodoItem.belongsTo(models.Todo, {
+      as: 'todo',
+      foreignKey: 'todoId'
+    })
+  };
+  return TodoItem;
+};
